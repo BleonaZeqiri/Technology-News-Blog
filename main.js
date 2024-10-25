363c7bf8ed5dd6b58a7451d71e7dfade6404d159
Bleona <zbleona9@gmail.com>
2024-10-26 00:13:07 +0200

    Added navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburgerMenu = document.querySelector(".hamburger-menu");
+//   const mobileMenu = document.querySelector(".mobile-menu");
+
+//   hamburgerMenu.addEventListener("click", function () {
+//     mobileMenu.classList.toggle("hidden");
+//   });
+// });
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("active");
+}
